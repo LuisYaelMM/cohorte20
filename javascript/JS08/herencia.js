d28a0d7383d50eb417644a50f504b5a988f0c912
yael <luya12@gmail.com>
2022-11-11 17:49:27 -0600

    node y gitignore

--- javascript/JS08/herencia.js
+++ javascript/JS08/herencia.js
@@ -1,39 +1,46 @@
 class Persona{
-    constructor(nombre, apellido){
-        this._nombre=nombre;
-        this._apellido=apellido;
-    }
+  constructor(nombre, apellido) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+  }
 
-    set nombre (nombre){
-        this._nombre=nombre;
-    }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
 
-    set apellido(apellido){
-        this._apellido=apellido;
-    }
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
 
-    nombreCompleto(){
-        return this._nombre + " "+ this._apellido;
-    }
-};
-
-class Empleado extends Persona{
-    constructor(nombre, apellido,empresa){
-        super(nombre, apellido);
-        this._empresa=empresa;
-    }
-
-    set (empresa){
-        this._empresa=empresa;
-    }
-
-    get empresa(){
-        return this._empresa;
-    }
+  nombreCompleto() {
+    return this._nombre + " " + this._apellido;
+  }
+  toString(){
+    return this.nombreCompleto();
+  }
 }
 
-let p1= new Persona("Hugo", "Sanchez");
+class Empleado extends Persona {
+  constructor(nombre, apellido, empresa) {
+    super(nombre, apellido);
+    this._empresa = empresa;
+  }
+
+  set(empresa) {
+    this._empresa = empresa;
+  }
+
+  get empresa() {
+    return this._empresa;
+  }
+  nombreCompleto() {
+    return super.nombreCompleto() + " " + this._empresa;
+  }
+}
+
+let p1 = new Persona("Hugo", "Sanchez");
 console.log(p1.nombreCompleto());
 
-let e1=new Empleado("Juan","Perez","coppel");
-console.log(e1.nombreCompleto()+" "+e1.empresa);
+let e1 = new Empleado("Juan", "Perez", "coppel");
+console.log(e1.nombreCompleto());
+console.log(e1.toString());
